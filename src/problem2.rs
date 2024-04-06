5b5a2b7913034d628787de58d30ab7cb910820f5
Ben Letchford <me@benletchford.com>
2024-04-06 23:05:31 +1100

    Problem 2 solved

--- src/problem2.rs
+++ src/problem2.rs
@@ -1,3 +1,18 @@
 pub fn answer() -> i32 {
-    0
+    let mut a = 1;
+    let mut b = 2;
+
+    let mut running_even_sum = b;
+    let mut ab = 0;
+    while ab < 4000000 {
+        ab = a + b;
+        a = b;
+        b = ab;
+
+        if ab % 2 == 0 {
+            running_even_sum += ab;
+        }
+    }
+
+    running_even_sum
 }
