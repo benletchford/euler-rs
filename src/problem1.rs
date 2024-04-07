ebe783622845fb8508fd637092aa33fb46dd34de
Ben Letchford <me@benletchford.com>
2024-04-07 16:36:19 +1000

    Restructure to use module level tests

--- src/problem1.rs
+++ src/problem1.rs
@@ -1,3 +1,10 @@
+/*!
+## Problem 1. Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 pub fn answer() -> i32 {
     let n = 1000;
     let mut running_sum = 0;
@@ -8,4 +15,14 @@ pub fn answer() -> i32 {
     }
 
     running_sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_answer() {
+        assert_eq!(answer(), 233168)
+    }
 }
--- src/problem3.rs
+++ src/problem3.rs
@@ -1,3 +1,10 @@
+/*!
+## Problem 3. Largest Prime Factor
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143?
+*/
+
 use crate::math;
 
 pub fn answer() -> u64 {
@@ -11,4 +18,15 @@ pub fn answer() -> u64 {
     }
 
     0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[ignore]
+    #[test]
+    fn test_answer() {
+        assert_eq!(answer(), 6857)
+    }
 }
